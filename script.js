b8399a8af5738df7331db1727fa78a0bd297492e
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2025-01-28 19:33:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if (size <= 0 || !str) {
+    return [];
+  }
+
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.substring(i, i + size));
+  }
+  return result;
 }
 
 // Do not change the code below
